79bf5719299e3e620e43cce7a9cbaca9fec9bb1e
vincegirard <vincent.girard01@gmail.com>
2021-03-26 13:36:24 -0400

    ajout du paramètre autocommit de connection

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -3,7 +3,7 @@ from pymysql import connect
 
 class DataBase:
     def __init__(self, host, user, password, db):
-        self.connection = connect(host=host, user=user, passwd=password, db=db)
+        self.connection = connect(host=host, user=user, passwd=password, db=db,autocommit = True)
 
     def cursor(self):
         return self.connection.cursor()
